b2b5c18bddced74075964aa86fa10dc1216c4c65
gavinning <gavinning@qq.com>
2020-05-06 11:54:42 +0800

    1.0.1

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function generate(length = 4) {
+    let i = 0
+
     if (length > 6) {
         length = 6
     }
 
     let result
     do {
+        i++
         result = Math.random().toString().slice(2, 2 + length)
+        console.log(i, result)
     }
-    while(result.length !== length)
+    while(result.length !== length && i < 100)
 
     return result
 }
